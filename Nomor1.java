d381c2bc1dc5a5d799538b036c2ac2909a03d17b
Qorrykharismayani <melloflouscyz24@gmail.com>
2024-10-24 08:43:20 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,29 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class soal1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("MASUKAN TOTAL JAM LEMBUR: ");
+        int jamLembur = scanner.nextInt();
+        
+        double honorLembur = hitungHonorLembur(jamLembur);
+        
+        System.out.println("TOTAL JAM LEMBUR: " + jamLembur);
+        System.out.println("HONOR LEMBUR: " + honorLembur);
+    }
+    
+    public static double hitungHonorLembur(int jamLembur) {
+        double honor;
+        if (jamLembur <= 16) {
+            honor = jamLembur * 10000;
+        } else {
+            honor = 16 * 10000 + (jamLembur - 16) * 15000;
+        }
+        return honor;
     }
 }
+
+        
+
+        
